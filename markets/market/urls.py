d791768a697affd1fe07f779999f35c22b66322b
mafuqiang <woshimaxiaoqiang@163.com>
2020-04-27 23:33:10 +0800

    some modify and delete

--- markets/market/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('login',views.login,name='login'),
-    path('add_product',views.add_product,name='add_product'),
-    path('delete/<slug:productid>',views.delete_product,name='delete_product'),
-]
